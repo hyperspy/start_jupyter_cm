4cc824bc8c94a7d435fbd7d5393bccee79b87ccf
Francisco de la Peña <frandelapena@gmail.com>
2016-02-15 16:16:53 +0000

    Add icons to GNOME entries.

--- start_jupyter_cm/gnome.py
+++ start_jupyter_cm/gnome.py
@@ -1,5 +1,6 @@
 import os
 import stat
+from subprocess import call
 
 NPATH = os.path.expanduser("~/.local/share/nautilus")
 SPATH = os.path.join(NPATH, "scripts")
@@ -12,6 +13,10 @@ def add_jupyter_here():
     if not os.path.exists(SPATH):
         os.makedirs(SPATH)
 
+    logo_path = os.path.expandvars(os.path.join(
+        os.path.dirname(__file__), 'icons'))
+    logos = {'qtconsole': os.path.join(logo_path, 'jupyter-qtconsole.png'),
+             'notebook': os.path.join(logo_path, 'jupyter.png')}
     for terminal in ["qtconsole", "notebook"]:
         script_path = os.path.join(SPATH, "Jupyter %s here" % terminal)
         if not os.path.exists(script_path):
@@ -19,7 +24,10 @@ def add_jupyter_here():
                 f.write("#!/bin/sh\njupyter-%s" % terminal)
             st = os.stat(script_path)
             os.chmod(script_path, st.st_mode | stat.S_IEXEC)
-            print("Jupyter %s here created." % terminal)
+            print('Jupyter %s here created.' % terminal)
+            call(['gvfs-set-attribute', '-t', 'string', '%s' % script_path,
+                  'metadata::custom-icon', 'file://%s' % logos[terminal]])
+
 
 def remove_jupyter_here():
     for terminal in ["qtconsole", "notebook"]:
