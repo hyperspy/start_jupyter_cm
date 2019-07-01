ef8ca8f01e726628b6e331251150b32a3fde1a2f
Eric Prestat <eric.prestat@gmail.com>
2019-07-01 18:19:40 +0100

    Put setting the icon in a try, except block:

--- start_jupyter_cm/gnome.py
+++ start_jupyter_cm/gnome.py
@@ -54,8 +54,12 @@ def add_jupyter_here():
                 f.write(script % (PATH, terminal, PATH, terminal))
             st = os.stat(script_path)
             os.chmod(script_path, st.st_mode | stat.S_IEXEC)
-            call(['gio', 'set', '-t', 'string', '%s' % script_path,
-                  'metadata::custom-icon', 'file://%s' % logos[terminal]])
+            try:
+                call(['gio', 'set', '-t', 'string', '%s' % script_path,
+                      'metadata::custom-icon', 'file://%s' % logos[terminal]])
+            except:
+                # Skip in case setting the icon breaks creating the shortcut
+                pass
             print('Jupyter %s here%s created.' % (terminal, CONDA_ENV_LABEL))
 
 
