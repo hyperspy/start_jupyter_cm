d4893bf0b819479416dcd8ac159f4aaeab16dbf9
Douglas Lowe <10961945+douglowe@users.noreply.github.com>
2019-09-30 16:27:42 +0100

    Added instructions for creating OSX Automater scripts.

--- start_jupyter_cm/macos.py
+++ start_jupyter_cm/macos.py
@@ -1,7 +1,7 @@
 import os, sys
 import shutil
 
-from .utils import get_environment_label
+from utils import get_environment_label
 
 NPATH = os.path.expanduser("~/Library/Services/")
 PATH = "%s/bin"%sys.exec_prefix
@@ -319,3 +319,48 @@ def remove_jupyter_here():
         if os.path.exists(script_path):
             shutil.rmtree(script_path)
             print("Jupyter %s here%s removed." % (terminal, CONDA_ENV_LABEL))
+
+
+
+'''
+Guide for building this script using Automator.
+
+This guide was originally based on this article: https://davidwalsh.name/mac-context-menu
+
+1) Start Automator (either through the Applications folder, or spotlight search)
+
+2) Open the "File" dropdown menu, and select "New"
+
+3) Choose the "Quick Action" document type (this is represented by the cog icon, 
+and used to be called "Service")
+
+4) In the "Library" menu on the left chose "Utilities", and then double-click on
+"Run Shell Script" in the menu 2nd on the left. This will open a "Run Shell Script" 
+panel in the right pane.
+
+5) Using the dropdown menus in the uppermost panel in the right pane set the workflow to
+receive current "files or folders" in "finder".
+
+6) In top-right corner of the "Run Shell Script" panel, set Pass Input "as arguments".
+
+7) Write Bash script, using this template (substitute for the anaconda install path, and 
+swap out jupyter-notebook for your python application of choice):
+#!/bin/bash
+if [[ -d $1 ]] ; then
+    cd "$1"
+    current_file="None"
+else
+    current_path=$(dirname "$1")
+    current_file=$(basename "$1")
+    cd "$current_path"
+fi
+
+if [[ $current_file == *.ipynb ]] ; then
+    [path to anaconda install]/bin/jupyter-notebook "$current_file"
+else
+    [path to anaconda install]/bin/jupyter-notebook
+fi
+
+8) Save the script (using a descriptive name), and test it as described in the README file.
+'''
+
