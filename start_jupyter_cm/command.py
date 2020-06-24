f2553daf39f457990537a0d03607b1634b5e25b1
Eric Prestat <eric.prestat@gmail.com>
2020-06-24 10:17:37 +0100

    Add option to select a file manager.

--- start_jupyter_cm/command.py
+++ start_jupyter_cm/command.py
@@ -6,6 +6,7 @@ from start_jupyter_cm import __version__
 parser = argparse.ArgumentParser(
     description='Add or remove context menu entries to start Jupyter applications in a given directory')
 parser.add_argument('--remove', action="store_true", help='remove the entries')
+parser.add_argument('-f', '--file_manager', action="store", help='select a specific file manager (Linux only)')
 parser.add_argument('--version', action='version', version='%(prog)s {version}'.format(version=__version__))
 
 args = parser.parse_args()
@@ -13,17 +14,20 @@ args = parser.parse_args()
 
 def _run():
     global args
+    params = []
     if os.name == "nt":
         from start_jupyter_cm.windows import (add_jupyter_here,
-                                            remove_jupyter_here)
+                                              remove_jupyter_here)
     elif os.uname().sysname == "Darwin":
         from start_jupyter_cm.macos import (add_jupyter_here,
                                             remove_jupyter_here)
     else:
         from start_jupyter_cm.linux import (add_jupyter_here,
                                             remove_jupyter_here)
+        if args.file_manager:
+            params.append(args.file_manager)
 
     if args.remove:
-        remove_jupyter_here()
+        remove_jupyter_here(*params)
     else:
-        add_jupyter_here()
+        add_jupyter_here(*params)
--- start_jupyter_cm/linux.py
+++ start_jupyter_cm/linux.py
@@ -5,18 +5,6 @@ import shutil
 
 from .utils import get_environment_label
 
-MANAGER_CONFIG_PATH = ""
-
-if shutil.which("nautilus"):
-    MANAGER_CONFIG_PATH = os.path.expanduser("~/.local/share/nautilus")
-if shutil.which("caja"):
-    MANAGER_CONFIG_PATH = os.path.expanduser("~/.config/caja")
-
-if MANAGER_CONFIG_PATH == "":
-    print("Only 'nautilus' and 'caja' are supported "
-                              "file manager on linux.")
-
-SPATH = os.path.join(MANAGER_CONFIG_PATH, "scripts")
 PATH = "%s/bin"%sys.exec_prefix
 CONDA_ENV_LABEL = get_environment_label()
 
@@ -39,20 +27,35 @@ for folder in folders:
 
 """
 
-def check_supported_file_manager():
-    if not os.path.exists(MANAGER_CONFIG_PATH):
+def check_supported_file_manager(manager_file_path):
+    if len(manager_file_path) == 0:
         print("Nothing done. Currently only 'Nautilus' and 'Caja' are "
               "supported file manager.")
         return False
     return True
 
 
-def add_jupyter_here():
-    if not check_supported_file_manager:
-        return
+def get_file_manager_config(file_manager=None):
+    file_manager_config = {}
     
-    if not os.path.exists(SPATH):
-        os.makedirs(SPATH)
+    if shutil.which("nautilus"):
+        file_manager_config['nautilus'] = os.path.expanduser("~/.local/share/nautilus")
+    if shutil.which("caja"):
+        file_manager_config['caja'] = os.path.expanduser("~/.config/caja")
+
+    if file_manager is not None:
+        if file_manager not in file_manager_config.keys():
+            print("File manager '%s' not installed or not supported." % file_manager)
+            return {}
+        return {file_manager:file_manager_config[file_manager]}
+    else:
+        return file_manager_config
+
+
+def add_jupyter_here(file_manager=None):
+    manager_config_path = get_file_manager_config(file_manager)
+    if not check_supported_file_manager(manager_config_path):
+        return
 
     logo_path = os.path.expandvars(os.path.join(
         os.path.dirname(__file__), 'icons'))
@@ -62,26 +65,37 @@ def add_jupyter_here():
 
     python_exec = shutil.which("python")
 
-    for terminal in ["qtconsole", "notebook", "lab"]:
-        script_path = os.path.join(SPATH, "Jupyter %s here%s" % (
-                terminal, CONDA_ENV_LABEL))
-        if (not os.path.exists(script_path) and
-            shutil.which("jupyter-%s" % terminal)):
-            with open(script_path, "w") as f:
-                f.write(script % (python_exec, PATH, terminal, PATH, terminal))
-            st = os.stat(script_path)
-            os.chmod(script_path, st.st_mode | stat.S_IEXEC)
-            call(['gio', 'set', '-t', 'string', '%s' % script_path,
-                  'metadata::custom-icon', 'file://%s' % logos[terminal]])
-            print('Jupyter %s here%s created.' % (terminal, CONDA_ENV_LABEL))
+    for name, path in manager_config_path.items():
+        print("File manager: %s" %name)
+        scripts_folder_path = os.path.join(path, "scripts")
+    
+        if not os.path.exists(scripts_folder_path):
+            os.makedirs(scripts_folder_path)
+        for terminal in ["qtconsole", "notebook", "lab"]:
+            script_path = os.path.join(scripts_folder_path, "Jupyter %s here%s" % (
+                    terminal, CONDA_ENV_LABEL))
+            if (not os.path.exists(script_path) and
+                shutil.which("jupyter-%s" % terminal)):
+                with open(script_path, "w") as f:
+                    f.write(script % (python_exec, PATH, terminal, PATH, terminal))
+                st = os.stat(script_path)
+                os.chmod(script_path, st.st_mode | stat.S_IEXEC)
+                call(['gio', 'set', '-t', 'string', '%s' % script_path,
+                      'metadata::custom-icon', 'file://%s' % logos[terminal]])
+                print('Jupyter %s here%s created.' % (terminal, CONDA_ENV_LABEL))
 
 
-def remove_jupyter_here():
-    if not check_supported_file_manager:
+def remove_jupyter_here(file_manager=None):
+    manager_config_path = get_file_manager_config(file_manager)
+    if not check_supported_file_manager(manager_config_path):
         return
-    for terminal in ["qtconsole", "notebook", "lab"]:
-        script_path = os.path.join(SPATH, "Jupyter %s here%s" %(
-                terminal, CONDA_ENV_LABEL))
-        if os.path.exists(script_path):
-            os.remove(script_path)
-            print("Jupyter %s here%s removed." % (terminal, CONDA_ENV_LABEL))
+
+    for name, path in manager_config_path.items():
+        print("File manager: %s" %name)
+        scripts_folder_path = os.path.join(path, "scripts")
+        for terminal in ["qtconsole", "notebook", "lab"]:
+            script_path = os.path.join(scripts_folder_path, "Jupyter %s here%s" %(
+                    terminal, CONDA_ENV_LABEL))
+            if os.path.exists(script_path):
+                os.remove(script_path)
+                print("Jupyter %s here%s removed." % (terminal, CONDA_ENV_LABEL))
