9df620cf29913fde9841ddfa8a0bbd09312310be
Eric Prestat <eric.prestat@gmail.com>
2021-04-10 13:21:29 +0100

    Add python 3.9 to classifiers.

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Development Status :: 4 - Beta",
         "Environment :: Console",
         "License :: OSI Approved :: BSD License",
